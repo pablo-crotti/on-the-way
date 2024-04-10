b0336de345a533e64630f213fb280234ea0f497e
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-04-10 09:50:48 +0200

    axios

---
+++ app/api-test/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page() {
+    return(
+        <div>
+            <h1 className="text-white">API Test</h1>
+            <p className="text-white">This is a test of the API</p>
+        </div>
+    )
+}
